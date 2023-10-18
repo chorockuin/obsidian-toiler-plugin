855ff4477b41f0389670ab3ee772aa83ce35ec5e
chorockuin <chorockuin@lginnotek.com>
2023-10-18 15:15:01 +0900

    Add toil_excel_file
    read excel file and extract data to json data format
    create MD note file from template file
    modify frontmatter and body of MD note from json data
    update MD note file
    -------------------------------------------------|
    * Subject Rule
      - English only
      - Capitalize first character
      - 50 characters or less
      - Don't use '.'
      - Begin with below word only
        - Add : new features
        - Update : feature
        - Remove : feature
        - Refactor : feature's structure not testcase
        - Fix : bug
        - Verify : feature with testcase
    ------------------------------------------------------------------------------|
    * Body Rule
      - 79 characters or less per line
      - Describe 'what' and 'why' not 'how'

--- main.ts
+++ main.ts
@@ -1,4 +1,8 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, TFile } from 'obsidian';
+// nvm install xlsx
+import * as XLSX from 'xlsx';
+// npm install --save-dev @types/js-yaml
+import * as YAML from 'js-yaml';
 
 // Remember to rename these classes and interfaces!
 
@@ -28,6 +32,21 @@ export default class MyPlugin extends Plugin {
 		const statusBarItemEl = this.addStatusBarItem();
 		statusBarItemEl.setText('Status Bar Text');
 
+        this.addRibbonIcon('document', 'Toil', async () => {
+            const input = document.createElement('input');
+            input.type = 'file';
+            input.onchange = async (e) => {
+				const input_files = (e.target as HTMLInputElement).files;
+				if (input_files && input_files.length > 0) {
+					const input_file = input_files[0];
+					await this.toil_excel_file(input_file);
+				} else {
+					console.warn("no files selected");
+				}
+            };
+            input.click();
+        });
+		
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
 			id: 'open-sample-modal-simple',
@@ -88,6 +107,139 @@ export default class MyPlugin extends Plugin {
 
 	async saveSettings() {
 		await this.saveData(this.settings);
+	}
+
+	// note: TFile -> md file
+	// content: string -> whole string content from md file
+	// frontmatter: string -> yaml string from the content
+	// body: string -> string excluding the frontmatter from the content
+    async toil_excel_file(excel_file: File) {
+		try {
+			const reader = new FileReader()
+			reader.onload = async (e) => {
+				const data = new Uint8Array((e.target as FileReader).result as ArrayBuffer)
+				const workbook = XLSX.read(data, {type: 'array'});
+				const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+				const json_table = XLSX.utils.sheet_to_json(worksheet);
+				for (let data of json_table) {
+					await this.process_data(data)
+					break
+				}
+			}
+			reader.readAsArrayBuffer(excel_file)
+		} catch (e) {
+			console.error(e.message)
+			throw e
+		}
+    }
+
+	async process_data(data: any) {
+		const note_path = data["Name"] + ".md"
+		const note = await this.create_note_from_template("templates/book.md", note_path)
+		const content = await this.read_content_from_note(note)
+		const frontmatter = this.read_frontmatter_from_content(content)
+		
+		frontmatter.title = data["Name"]
+		frontmatter.created = "2023-10-05 16:18"
+		const genre_map: { [key: string]: string } = {
+			"Algorithm": "알고리즘",
+			"Data Engineering": "데이터",
+			"Engineering": "엔지니어링",
+			"Essay": "에세이",
+			"Hardware": "하드웨어",
+			"Infrastructure": "인프라",
+			"Language": "언어",
+			"Math": "수학",
+			"Network": "네트워크",
+			"Quantum": "양자",
+			"Robotics": "로보틱스",
+			"Security": "보안",
+			"System": "시스템"
+		}
+		frontmatter.genre = [genre_map[data["Type"]] || data["Type"]]
+		frontmatter.release = parseInt(data["Year"])
+		frontmatter.authors = data["Author"].split(", ").map((s: string) => s.trim());
+		frontmatter.publishers = [data["Publisher"]]
+		frontmatter.rating = ""
+		frontmatter.status = "todo"
+
+		const body = data["Link"]
+		
+		this.update_note_from_content(note, this.update_content_from_body(this.update_content_from_frontmatter(content, frontmatter), body))
+	}
+
+	async create_note_from_template(template_path: string, note_path: string) {
+		try {
+			const template = this.app.vault.getAbstractFileByPath(template_path);
+			if (!(template instanceof TFile)) {
+				throw new Error("template is not TFile format")
+			}	
+
+			const template_contents = await this.app.vault.read(template)
+			
+			await this.app.vault.create(note_path, template_contents)
+			const note = this.app.vault.getAbstractFileByPath(note_path)
+			if (!(note instanceof TFile)) {
+				throw new Error("note is not TFile format")
+			}
+			return note
+		} catch (e) {
+			console.error(e.message)
+			throw e
+		}
+	}
+
+	async read_content_from_note(note: TFile) {	
+		try {
+			const content = await this.app.vault.read(note)
+			return content
+		} catch (e) {
+			console.error(e.message)
+			throw e
+		}
+	}
+
+	async update_note_from_content(note: TFile, content: string) {
+		try {
+			await this.app.vault.modify(note, content)
+		} catch (e) {
+			console.error(e.message)
+			throw e
+		}
+	}
+
+	read_frontmatter_from_content(content: string) {
+		try {
+			const match = content.match(/---\n([\s\S]+?)\n---/)
+			if (!match) {
+				throw new Error("no yaml frontmatter found.")
+			}
+			const frontmatter = YAML.load(match[1]) as Record<string, any>
+			return frontmatter
+		} catch (e) {
+			console.error(e.message)
+			throw e
+		}
+	}
+
+	update_content_from_frontmatter(content: string, frontmatter: Record<string, any>) {
+		try {
+			const updated_frontmatter = YAML.dump(frontmatter);
+			const match = content.match(/---\n([\s\S]+?)\n---/)
+			if (!match) {
+				throw new Error("no yaml frontmatter found.")
+			}
+			const updated_content = content.replace(match[0], `---\n${updated_frontmatter}---`)
+			return updated_content
+		} catch (e) {
+			console.error(e.message)
+			throw e
+		}
+	}
+
+	update_content_from_body(content: string, body: string) {
+		const updated_content = content + "\n" + body
+		return updated_content
 	}
 }
 
